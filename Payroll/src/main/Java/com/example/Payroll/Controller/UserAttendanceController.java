33f3d4ea6566e91016390738601f481684c5582f
2ez4zero <paulinokenn@gmail.com>
2025-07-15 14:00:41 +0800

    userController

---
+++ Payroll/src/main/Java/com/example/Payroll/Controller/UserAttendanceController.java
@@ -0,0 +1,26 @@
+package com.example.Payroll.Controller;
+
+import com.example.Payroll.Entity.User;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/userAttendance")
+public class UserAttendanceController {
+
+    @RequestMapping("")
+    public String showUserAttendance(HttpSession session, Model model) {
+        User user = (User) session.getAttribute("user");
+
+        if (user == null) {
+            return "redirect:/login";
+        }
+
+        model.addAttribute("user", user);
+
+
+        return "userAttendance";
+    }
+}
